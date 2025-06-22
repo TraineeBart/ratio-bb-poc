bf6656bb03bcee25719e40edc6729bf2f7c7f5db
TraineeBart <bartdewijs+github@gmail.com>
2025-06-22 14:46:39 +0200

    test: add src to PYTHONPATH in kucoin_fetcher tests

--- tests/test_kucoin_fetcher.py
+++ tests/test_kucoin_fetcher.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 import pytest
 import requests
 from requests.exceptions import HTTPError, Timeout
